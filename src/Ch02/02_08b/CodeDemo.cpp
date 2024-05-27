2777350eda4114ec606118df67796a09d1ad6233
NBalleBS1 <144804295+NBalleBS1@users.noreply.github.com>
2024-05-27 11:01:52 +0000

    cont

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -4,7 +4,27 @@
 
 #include <iostream>
 
+// #define AGE_LENGTH 4
+
 int main(){
+    const size_t AGE_LENGTH = 4;
+    int age[AGE_LENGTH];
+    float temperature[] = {31.5f, 32.7f, 38.9f};
+
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
+
+    std::cout << "The Age array has " << AGE_LENGTH << " elements" << std::endl;
+    std::cout << "Age[0] = " << age[0] << std::endl;
+    std::cout << "Age[1] = " << age[1] << std::endl;
+    std::cout << "Age[2] = " << age[2] << std::endl;
+    std::cout << "Age[3] = " << age[3] << std::endl;
+    std::cout << std::endl;
+    std::cout << "Temp[0] = " << temperature[0] << std::endl;
+    std::cout << "Temp[1] = " << temperature[1] << std::endl;
+    std::cout << "Temp[2] = " << temperature[2] << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,6 +6,20 @@
 #include <cstdint>
 
 int main(){
+    int fahrenheit = 100;
+    int celsius;
+
+    celsius = ((float)5 / 9.0f) * (fahrenheit - 32);
+
+    std::cout << "Fahrenheit: " << fahrenheit << std::endl;
+    std::cout << "Celsius: " << celsius << std::endl;
+    std::cout << std::endl;
+
+    float weight = 10.99;
+
+    std::cout << "Float          : " << weight << std::endl;
+    std::cout << "Integer Part   : " << (int)weight << std::endl;
+    std::cout << "Fractional Part: " << (int)((weight - (int)weight) * 10000) << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
